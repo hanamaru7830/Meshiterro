18dfab7504083e0896603a700972ded6bf132d99
hanamaru7830 <xwsmiexw@gmail.com>
2021-09-15 12:58:52 +0000

    [Finish]1-2

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #このモデルには、deviseで利用する機能が記述されています。(デフォルト)
+
+         #パスワードの正確性を検証, ユーザ登録や編集、削除
+  devise :database_authenticatable, :registerable,
+        #パスワードをリセット, ログイン情報を保存, emailのフォーマットなどのバリデーション
+         :recoverable, :rememberable, :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  #deviseを使用する際にURLとしてusersを含むことを示す
+  devise_for :users
+  #get 'top' => 'homes#top'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
